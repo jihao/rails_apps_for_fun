c48d04d17a0419e0f0cf95efb9634a17e86bfe23
jihao <jacky.jihao@gmail.com>
2011-09-06 23:30:04 +0800

    using bootstrap css from twitter to style/customize the login pages of devise, this is also an experiment

---
+++ login_bootstrap_style/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoginBootstrapStyle::Application.config.secret_token = 'c8b56933c6a57a3d5b1fc0f355fd428d9d1dc1fc4c570a0918647c8e96fafa971d6ac140a5b15ac7cd12abc7e930992c681fc45e202407775a9c477a75f18ae5'
