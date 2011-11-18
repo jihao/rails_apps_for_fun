18952dd1baea03930ca9837aa353d89c469b0668
jihao <jacky.jihao@gmail.com>
2011-11-18 23:41:07 +0800

    test scope

--- test_startup_app/app/models/client.rb
+++ test_startup_app/app/models/client.rb
@@ -2,4 +2,11 @@ class Client < ActiveRecord::Base
   has_one :address
   has_many :orders
   has_and_belongs_to_many :roles
+  
+  scope :week, where("created_at > ?", DateTime.now-7.days).limit(5)
+  
+  scope :last_week, lambda { where("created_at < ?", Time.zone.now ) }
+  
+  scope :one_week_before, lambda { |time| where("created_at < ?", time) }
+    
 end
